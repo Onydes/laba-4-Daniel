c291e22117a9e4f2494f4cd42563f94c64ac725f
Onydes <pud8573@gmail.com>
2024-09-30 18:36:50 +0300

    Update task5.js

--- tasks/task5.js
+++ tasks/task5.js
@@ -3,11 +3,19 @@
 
 
 function countOccurrences(str, char) {
-  //Ваш код
+function countOccurrences(str, char) {
+  let count = 0; // Ініціалізуємо лічильник
+  for (let i = 0; i < str.length; i++) { // Проходимо по кожному символу рядка
+    if (str[i] === char) { // Якщо символ у рядку дорівнює заданому символу
+      count++; // Збільшуємо лічильник
+    }
+  }
+  return count; // Повертаємо кількість входжень
 }
 
 // Приклади використання:
 console.log(countOccurrences("hello world", 'o')); // Виведе: 2
 console.log(countOccurrences("banana", 'a'));       // Виведе: 3
 
-module.exports = countOccurrences;
+module.exports = countOccurrences; // Експортуємо функцію для використання в інших файлах
+
