3ef7b1508badd5d42eb4fc6593f0fbe6fe3699ca
Onydes <pud8573@gmail.com>
2024-09-30 18:35:00 +0300

    Update task3.js

--- tasks/task3.js
+++ tasks/task3.js
@@ -2,10 +2,16 @@
 // повертає їхню суму. Використовуйте цикл для обчислення суми.
 
 function sumArray(numbers) {
-  // Ваш код тут
+function sumArray(numbers) {
+  let total = 0; // Ініціалізуємо змінну для зберігання суми
+  for (let i = 0; i < numbers.length; i++) { // Проходимо по всіх елементах масиву
+    total += numbers[i]; // Додаємо кожен елемент до загальної суми
+  }
+  return total; // Повертаємо загальну суму
 }
 
 console.log(sumArray([1, 2, 3, 4, 5])); // Виведе: 15
 console.log(sumArray([10, 20, 30]));    // Виведе: 60
 
-module.exports = sumArray;
+module.exports = sumArray; // Експортуємо функцію для використання в інших файлах
+
