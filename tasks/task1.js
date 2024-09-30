8bc6cddf96daea4c4bfb94d9b4e938f11baf656f
Onydes <pud8573@gmail.com>
2024-09-30 18:32:30 +0300

    Update task1.js

--- tasks/task1.js
+++ tasks/task1.js
@@ -2,10 +2,27 @@
 // на певний символ, наприклад *.
 
 function replaceVowels(str) {
-  // Ваш код тут
+function replaceVowels(str) {
+  // Список гласных символов
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+  
+  // Преобразуем строку в массив символов и заменяем гласные
+  let result = '';
+  
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i])) {
+      result += '*'; // Если символ гласный, заменяем на *
+    } else {
+      result += str[i]; // Если не гласный, добавляем символ без изменений
+    }
+  }
+
+  return result;
 }
 
 console.log(replaceVowels("hello world")); // Виведе: "h*ll* w*rld"
 console.log(replaceVowels("Javascript"));  // Виведе: "J*v*scr*pt"
 
 module.exports = replaceVowels;
+
+
